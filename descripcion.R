2c51d52dd45a594048887ecd61f535ba92f1b500
alvarixmeil <alvarixeduardo@gmail.com>
2015-11-27 17:44:49 -0600

    ACA VA LA DESCRIPCION DE LAS SERIES TEMPORALES DE LAS CINCO BASES DE DATOS EN SPSS

---
+++ descripcion.R
@@ -0,0 +1,13 @@
+#las primeras bases de datos muestran las tendecias no estacionarias sin diferenciarlas 
+#hasta que en una serie se le aplica log para transformar la serie y se obtiene 
+#una forma en la cual al aplicarle la segunda diferenciacion como resultado se asemeja 
+#un comportamiento parecido
+#bueno esas serian para la serie  1 y 2
+#en la tercera serie se obtinen las graficas con tendencia estocastica 
+# y se le aplica log a la serie para transformarla y quitar la homocedasticidad
+# Para la cuarta serie se le aplicado otra serie de soportes que la grafica necesita para 
+#obtener las autocorrelaciones que serviran para el modelo arma
+#Aparte se le a aplicado una log para transformar la serie y por ultimo se incluye el modelo arma
+#ya diferenciada la serie
+#Luego para la ultima serie se obtuvo la grafica de las personas que viajan a otros destinos sin escalas 
+#a otro continente
